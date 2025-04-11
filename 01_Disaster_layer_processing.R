c07abcecf895c1aab39667b7a37fe00109a66d98
luisrua <luisruarodriguez@gmail.com>
2025-04-11 07:40:21 +0200

    first lines

---
+++ 01_Disaster_layer_processing.R
@@ -0,0 +1,70 @@
+
+# 01 DISASTER RASTER PROCESSING 
+# SETTINGS =====================================================================
+
+# Libraries
+library(terra)
+library(tidyterra)
+library(exactextractr)
+
+# Patths
+layers <- "C:/GIS/UNFPA GIS/Spatial Analysis Regional/Disaster_popestimates/layers/"
+
+# 1. IMPORT LAYERS =============================================================
+
+# IMPACT ZONES RASTERS
+req <- rast(paste0(layers,"GAR Atlas hazard layers/EQ/PGA_475y.grd"))
+rwind <- rast(paste0(layers,"GAR Atlas hazard layers/TC WIND/VIENTO_MUNDO_TR100_INT1.grd"))
+
+# for flood there is a raster for each country
+floodlist <- list.files(paste0(layers,"GAR Atlas hazard layers/FL/"), pattern = ".grd", full.names = TRUE)
+
+# ADMIN BOUNDARIES
+ab <- vect(paste0(layers,"ab/lac_ab_line_54034.gpkg"))
+
+# 2. CREATE IMPACT ZONES FOR EACH OF THE EVENTS ================================
+# Para inundaciones: 50cm
+# Para vientos huracanados: 178 m/h (el límite de categoría 3 en la categoría Safir-Simpson)
+# Para terremotos: 150cm/s2 (el límite usado en muchos reglamentos de construcción para definir la zona de amenaza intermedia
+                           
+# 2.1 EARTHQUAKE -----
+# The threshold facilitated by UNDDR us 150cm/s2 which is the limit used in building 
+# regulation to define intermediate threathen.
+eq_limit <- 150
+
+# Set projection for raster
+crs(req) <- "EPSG:4326"
+
+# Crop the raster within LAC Region
+ab_crop <- project(ab, "EPSG:4326")
+req <- crop(req,ab_crop)
+
+# Create the area that includes the Impact zone
+req_impact <- req >= eq_limit
+plot(req_impact)
+
+req_pol <- as.polygons(req_impact, dissolve = T)
+plot(req_pol)
+
+# And project back into Equal Area Projection
+req_pol <- project(req_pol, "ESRI:54034")
+
+# 2.2 WINDS ------
+# 178m/h this is the limit for CAT 3 in Safir - Simpson scale
+wind_limit <- 178
+
+# Set projection for raster
+crs(rwind) <- "EPSG:4326"
+
+# Crop the raster within LAC Region
+
+rwind <- crop(rwind,ab_crop)
+plot(rwind)
+
+# Create the area that includes the Impact zone
+rwind_impact <- rwind >= wind_limit
+plot(rwind_impact)
+
+rwind_pol <- as.polygons(rwind_impact, dissolve = T)
+plot(rwind_pol)
+
